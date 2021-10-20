6996073a69b0ef38243f6895fd2ac9725756570e
Meyank M <65050847+memeyankm@users.noreply.github.com>
2021-10-20 17:53:38 +0530

    Create subtract-two-numbers.py

---
+++ subtract-two-numbers.py
@@ -0,0 +1,7 @@
+# This code snippet subtracts num1 from num2
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+# Calculate the difference
+difference = num1 - num2
+# Output the results
+print('The difference b/w num1 and num2 is: ', difference)
